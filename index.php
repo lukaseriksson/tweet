4bfcd6757c72b7424d2aed61ac7ac7e0b58101c4
lukaseriksson <lukas.eriksson2@elev.ga.ntig.se>
2019-09-12 10:52:23 +0200

    lektion torsdag 12/9

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'include/dbinfo.php';
+try {
+    $dbh = new PDO(
+        'mysql:host=localhost;dbname=' . $database . '',
+         $user,
+          $password
+    );
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+$sth = $dbh->prepare('SELECT * FROM tweet
+            JOIN users
+            ON tweet.user_id = users.id');
+$sth->execute();
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+include 'views/index_layout.php';
+?>
