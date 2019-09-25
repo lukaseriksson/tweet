068358ca095d0c2e6fdb4ee1f9e2b5a4aefd7494
lukaseriksson <lukas.eriksson2@elev.ga.ntig.se>
2019-09-25 11:11:00 +0200

    asodinaokdjoakjksdopa okpa

--- tweet.php
+++ tweet.php
@@ -19,35 +19,6 @@ $sth = $dbh->prepare('SELECT tweet.*, users.name FROM tweet
             ON tweet.user_id = users.id
             WHERE tweet.id =' . $tweetId);
 $sth->execute();
-$result = $sth->fetch(PDO::FETCH_ASSOC);
-print_r($result);
+$row = $sth->fetch(PDO::FETCH_ASSOC);
+include 'views/tweet_layout.php';
 ?>
-
-<body>
-    <div class="container px-5">
-    <?php foreach ($result as $row): ?>
-        <section class="card mt-2">
-            <header class="card-header d-flex align-items-center">
-                <h5 class="card-title pl-4 mb-0">
-                    <?= $row['name'] ?>
-                    <small class="pl-4">
-                        <?= $row['created_at'] ?>
-                    </small>
-                </h5>
-            </header>
-            <a href="tweet.php?id=<?= $row['id'] ?>">
-                <div class="card-body">
-                    <p class="card-text">
-                        <?= $row['body'] ?>
-                    </p>
-                </div>
-            </a>
-            <footer class="card-footer d-flex justify-content-end">
-            </footer>
-        </section>
-    <?php endforeach ?>
-    </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
